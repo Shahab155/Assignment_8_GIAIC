46e952636e8c107706ec2f3dc5f4c8ef4ba3efa1
shahab2015 <166250566+shahab2015@users.noreply.github.com>
2024-11-22 01:48:17 -1100

    first comment

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let menuButton = document.querySelector(".menu-button");
 let navbar = document.querySelector(".nav-list");
-
+let inputField = document.querySelector("input");
+let textArea = document.querySelector("textarea")
 
 
 menuButton.addEventListener("click",()=>{
    navbar.classList.toggle("showNavbar")
  
 })
+
 
 
